3c1a72df2307a3c4b99e0a63f9bf6226dc752f31
Bartek <bartek.rozwarski@brightinventions.pl>
2020-05-13 09:29:38 +0200

    change path

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftMessages",
-            path: "Sources"
+            path: "Sources/SwiftMessages"
         ),
         .testTarget(
             name: "SwiftMessagesTests",
