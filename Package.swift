f4c7171dd38117527fbecfe0daf595bed6245c96
Bartek <bartek.rozwarski@brightinventions.pl>
2020-05-13 09:32:13 +0200

    tmp

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftMessages",
-            path: "Sources/SwiftMessages"
+            path: "Source"
         ),
         .testTarget(
             name: "SwiftMessagesTests",
